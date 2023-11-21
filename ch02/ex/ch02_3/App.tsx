e159fa04eb5cbd3d0ac4d9b976c09b362c5b7dba
mucollabo <mucollabo@gmail.com>
2023-11-22 00:43:40 +0900

    until p79

---
+++ ch02/ex/ch02_3/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import * as D from './src/data';
+
+const person = D.createRandomPerson();
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>{JSON.stringify(person, null, 2)}</Text>
+    </SafeAreaView>
+  );
+}
