74077a5ddfef909babe2b79c32343a438a2ff2e4
mucollabo <mucollabo@gmail.com>
2023-11-22 22:59:52 +0900

    until p92

--- ch02/ex/ch02_3/App.tsx
+++ ch02/ex/ch02_3/App.tsx
@@ -5,15 +5,22 @@
  * @format
  */
 
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-import * as D from './src/data';
+import React from 'react'
+import {SafeAreaView, ScrollView} from 'react-native'
+// import ClassComponent from './src/screens/ClassComponent'
+// import ArrowComponent from './src/screens/ArrowComponent'
+import Person from './src/screens/Person'
+import * as D from './src/data'
 
-const person = D.createRandomPerson();
+const people = D.makeArray(100).map(D.createRandomPerson)
+
 export default function App() {
+  const children = people.map(person => (
+    <Person key={person.id} person={person} />
+  ))
   return (
     <SafeAreaView>
-      <Text>{JSON.stringify(person, null, 2)}</Text>
+      <ScrollView>{children}</ScrollView>
     </SafeAreaView>
-  );
+  )
 }
